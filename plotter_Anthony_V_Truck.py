dc232c0120b38645e045b87ba62a8278b06557d9
Anthony Vu <AVu5557@student.pps.net>
2019-11-05 10:58:29 -0800

    Truck

---
+++ plotter_Anthony_V_Truck.py
@@ -0,0 +1,74 @@
+# Anthony Vu
+from madison_axi.axi import*
+initialize()
+from random import randrange
+from random import choice
+
+move_to(-250,0)
+#truck
+pen_down()
+for x in range(1,180):
+  move_forward(1)
+  turn_right(2)
+  
+#circle
+point_in_direction(360)
+pen_down()
+move_forward(150)
+for x in range(1,180):
+  move_forward(1)
+  turn_right(2)
+
+#rectangle
+point_in_direction(360)
+for x in range(1,3):
+ move_forward(50)
+ turn_left(90)
+
+move_forward(300)
+turn_left(90)
+move_forward(50)
+turn_left(90)
+move_forward(100)
+
+#window
+pen_up()
+point_in_direction(90)
+move_forward(50)
+turn_right(90)
+move_forward(150)
+pen_down()
+for x in range(1,4):
+ turn_left(90)
+ move_forward(50)
+
+#flag
+pen_up() 
+turn_left(90)
+move_forward(25)
+turn_left(90)
+move_forward(50)
+pen_down()
+move_forward(100)
+turn_right(90)
+move_forward(75)
+turn_right(90)
+move_forward(75)
+turn_right(90)
+move_forward(75)
+
+#shape_of_flag
+pen_up()
+turn_right(90)
+move_forward(37)
+turn_right(90)
+move_forward(37)
+pen_down()
+
+#shape
+poly = choice([2,3,5,6,8,9,10,12])
+size = randrange(5,20)
+for angle in range(0,poly):
+    move_forward(size)
+    turn_left(360/poly)
+cleanup()
